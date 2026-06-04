f2982531efbd2c98018406283b24f862d53cb7aa
ollily <426229+ollily@users.noreply.github.com>
2026-06-04 16:59:04 +0200

    #2: switch to php 8.4

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -16,26 +16,27 @@ EOF;
 
 $projRules = [
     // Rulsets
-    '@PSR2'                                         => true,
-    '@PSR12'                                        => true,
+    '@PSR2' => true,
+    '@PSR12' => true,
+    '@PHP8x4Migration' => true,
     // Customizing
-    'declare_strict_types'                          => true,
-    'no_trailing_comma_in_singleline'               => true,
-    'no_unused_imports'                             => true,
-    'protected_to_private'                          => false,
-    'modifier_keywords'                             => true,
-    'return_assignment'                             => true,
+    'declare_strict_types' => true,
+    'no_trailing_comma_in_singleline' => true,
+    'no_unused_imports' => true,
+    'protected_to_private' => false,
+    'modifier_keywords' => true,
+    'return_assignment' => true,
     // Blank lines / spacing
-    'class_attributes_separation'                   => [
+    'class_attributes_separation' => [
         'elements' => [
-            'const'        => 'one',
-            'method'       => 'one',
-            'property'     => 'one',
+            'const' => 'one',
+            'method' => 'one',
+            'property' => 'one',
             'trait_import' => 'none',
-            'case'         => 'none'
+            'case' => 'none'
         ]
     ],
-    'blank_line_before_statement'                   => [
+    'blank_line_before_statement' => [
         'statements' => [
             'declare',
             'exit',
@@ -48,7 +49,8 @@ $projRules = [
             'try'
         ]
     ],
-    'no_extra_blank_lines'                          => [
+    'no_unused_imports' => true,
+    'no_extra_blank_lines' => [
         'tokens' => [
             'attribute',
             'break',
@@ -65,20 +67,29 @@ $projRules = [
             'throw'
         ]
     ],
-    'statement_indentation'                         => ['stick_comment_to_next_continuous_control_statement' => true],
+    'ordered_imports' => [
+        'imports_order' => ['class', 'function', 'const'],
+        'sort_algorithm' => 'alpha'
+    ],
+    'single_import_per_statement' => [
+        'group_to_single_imports' => true
+    ],
+    'statement_indentation' => ['stick_comment_to_next_continuous_control_statement' => true],
     // Comments
-    'multiline_comment_opening_closing'             => true,
-    'no_empty_comment'                              => true,
-    'single_line_comment_spacing'                   => true,
-    'single_line_comment_style'                     => true,
-    'align_multiline_comment'                       => ['comment_type' => 'phpdocs_like'],
+    'multiline_comment_opening_closing' => true,
+    'no_empty_comment' => true,
+    'single_line_comment_spacing' => true,
+    'single_line_comment_style' => true,
+    'align_multiline_comment' => ['comment_type' => 'phpdocs_like'],
     // PHPDoc
-    'no_blank_lines_after_phpdoc'                   => true,
-    'no_empty_phpdoc'                               => true,
-    'phpdoc_add_missing_param_annotation'           => true,
-    'phpdoc_align'                                  => [
+    'no_blank_lines_after_phpdoc' => true,
+    'no_empty_phpdoc' => true,
+    'phpdoc_add_missing_param_annotation' => [
+        'only_untyped' => false,
+    ],
+    'phpdoc_align' => [
         'align' => 'vertical',
-        'tags'  => [
+        'tags' => [
             'method',
             'param',
             'property',
@@ -107,20 +118,21 @@ $projRules = [
             'psalm-method'
         ]
     ],
-    'phpdoc_annotation_without_dot'                 => true,
-    'phpdoc_indent'                                 => true,
-    'phpdoc_inline_tag_normalizer'                  => true,
-    'phpdoc_line_span'                              => ['property' => 'single', 'const' => 'single', 'method' => 'multi'],
-    'phpdoc_no_access'                              => true,
-    'phpdoc_no_alias_tag'                           => true,
-    'phpdoc_no_empty_return'                        => true,
-    'phpdoc_no_package'                             => true,
-    'phpdoc_no_useless_inheritdoc'                  => true,
-    'phpdoc_order'                                  => ['order' => ['param', 'return', 'throws']],
-    'phpdoc_param_order'                            => true,
-    'phpdoc_return_self_reference'                  => true,
-    'phpdoc_scalar'                                 => true,
-    'phpdoc_separation'                             => [
+    'phpdoc_annotation_without_dot' => true,
+    'phpdoc_indent' => true,
+    'phpdoc_inline_tag_normalizer' => true,
+    'phpdoc_line_span' => ['property' => 'single', 'const' => 'single', 'method' => 'multi'],
+    'phpdoc_no_access' => true,
+    'phpdoc_no_alias_tag' => true,
+    'phpdoc_no_duplicate_types' => true,
+    'phpdoc_no_empty_return' => true,
+    'phpdoc_no_package' => true,
+    'phpdoc_no_useless_inheritdoc' => true,
+    'phpdoc_order' => ['order' => ['param', 'return', 'throws']],
+    'phpdoc_param_order' => true,
+    'phpdoc_return_self_reference' => true,
+    'phpdoc_scalar' => true,
+    'phpdoc_separation' => [
         'groups' => [
             ['author', 'copyright', 'license'],
             ['category', 'package', 'subpackage'],
@@ -129,17 +141,26 @@ $projRules = [
             ['psalm-suppress', 'phpstan-ignore']
         ],
     ],
-    'phpdoc_single_line_var_spacing'                => true,
-    'phpdoc_summary'                                => true,
-    'phpdoc_tag_type'                               => true,
-    'phpdoc_to_comment'                             => ['ignored_tags' => ['phpstan-ignore', 'psalm-suppress']],
+    'phpdoc_single_line_var_spacing' => true,
+    'phpdoc_summary' => true,
+    'phpdoc_tag_casing' => true,
+    'phpdoc_tag_type' => true,
+    'phpdoc_to_comment' => ['ignored_tags' => ['phpstan-ignore', 'psalm-suppress']],
     'phpdoc_trim_consecutive_blank_line_separation' => true,
-    'phpdoc_trim'                                   => true,
-    'phpdoc_types'                                  => true,
-    'phpdoc_types_order'                            => true,
-    'phpdoc_var_annotation_correct_order'           => true,
-    'phpdoc_var_without_name'                       => true,
-    'header_comment'                                => ['header' => $headerText]
+    'phpdoc_trim' => true,
+    'phpdoc_types' => true,
+    'phpdoc_types_order' => true,
+    'phpdoc_var_annotation_correct_order' => true,
+    'phpdoc_var_without_name' => true,
+    // Risky
+    /* 
+      '@PHP8x2Migration:risky' => true,
+      'phpdoc_to_param_type' =>true,
+      'phpdoc_to_property_type' =>true,
+      'phpdoc_to_return_type' => true,
+     */
+    // Header
+    'header_comment' => ['header' => $headerText]
 ];
 
 $finder = PhpCsFixer\Finder::create()->in(__DIR__ . '/src')->in(__DIR__ . '/tests');
